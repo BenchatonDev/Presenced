bbd0bbfb752efcb55c1ff22d8c988b31fa807799
BenchatonDev <benchaton.annimation@gmail.com>
2026-07-23 23:11:01 +0200

    All relevent data IDed
    Something about my child checking logic isn't right but I can't see it :/

--- src/PS3/sman.py
+++ src/PS3/sman.py
@@ -4,27 +4,59 @@ PS3Data = {}
 
 class webMANParser(HTMLParser):
     checkTag = False
+    ignoreChild = 0
 
     def handle_starttag(self, tag, attrs):
+        if self.checkTag and tag != 'a': self.ignoreChild += 1; print(self.ignoreChild); return
         if tag != 'a': return
+        self.ignoreChild = 0
+
+        # Here we'll check for distinctive attributes of the tags we want
+        # to retrieve data from i.e. CPU temps, current game, etc
         attributes = dict(attrs)
 
         match attributes:
+            # Title ID
+            case {'target': '_blank'}:
+                self.checkTag = True
+                print("Game ID part")
+
+            # Title Name
+            case {'href': url} if url.startswith('http://google.com/'):
+                self.checkTag = True
+                print("Game Name part")
+
+            # Temps in Celsius
+            case {'href': '/cpursx.ps3?up'}:
+                self.checkTag = True
+                print("Celcius Temp Part")
+
+            # Temps in Fahrenheit
+            case {'href': '/cpursx.ps3?dn'}:
+                self.checkTag = True
+                print("Fahrenheit Temp Part")
+
+            # Fan Speed
+            case {'href': '/cpursx.ps3?mode'}:
+                self.checkTag = True
+                print("Fan Speed Part")
+
+            # Firmware version
             case {'href': '/setup.ps3'}:
                 self.checkTag = True
-                print("WOOOOOO")
-
-        print(attributes)
+                print("Firmware version Part")
 
     def handle_endtag(self, tag):
-            if self.checkTag:
-                return
+            if not self.checkTag: return
+            print(self.ignoreChild)
+            if tag != 'a': self.ignoreChild -= 1;
             
-            self.inAtag = False
+            self.checkTag = False
 
     def handle_data(self, data):
-        if not self.inAtag:
-            return
+        if not self.checkTag: return
+        if self.ignoreChild > 0: return
+
         print("Data     :", data)
 
 parser = webMANParser()
