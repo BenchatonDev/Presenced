3c094a92d5e74ec160123633f767660ef08b7731
BenchatonDev <benchaton.annimation@gmail.com>
2026-08-03 21:19:45 +0200

    New parser completed

--- src/PS3/SmanParser.py
+++ src/PS3/SmanParser.py
@@ -42,7 +42,7 @@ def SmanHTMLParser(html : str):
         PS3Data['TitleName'] = "XMB"
 
     # For the CPU and RSX temps we first isolate the IC's part from the <a> tag containing both
-    # then we use a second regular expression on the result to extract just the raw numbers
+    # Then we use a second regular expression on the result to extract just the raw numbers
     TempStr = SmanContent.find("a", href="/cpursx.ps3?up").text
     PS3Data['CPUTemp']['C'] = re.search("[0-9]+" ,re.search("CPU(.+?)C", TempStr).group(0)).group(0)
     PS3Data['RSXTemp']['C'] = re.search("[0-9]+" ,re.search("RSX(.+?)C", TempStr).group(0)).group(0)
@@ -51,91 +51,18 @@ def SmanHTMLParser(html : str):
     PS3Data['CPUTemp']['F'] = re.search("[0-9]+" ,re.search("CPU(.+?)F", TempStr).group(0)).group(0)
     PS3Data['RSXTemp']['F'] = re.search("[0-9]+" ,re.search("RSX(.+?)F", TempStr).group(0)).group(0)
 
+    # For fanspeed we just extract the number directly since it's the only one in the char sequence
+    TempStr = SmanContent.find("a", href="/cpursx.ps3?mode").text
+    PS3Data['FanSpeed'] = re.search("[0-9]+", TempStr).group(0)
+
+    # Firmware version is annoying, the string we are looking for is the first
+    # Segment of the sequence before a line break so we extract that, then we look
+    # For the first occurence of the firmware version which is alway X.XX formated,
+    # We only keep what comes after this sequence (including it self) and separate
+    # The string at each space char, since the firmware type comes right after the
+    # Version number we can then just join back the first 2 indexes of the split string
+    # To get the exact firmware version and type formated like this: X.XX CEX / DEX
+    TempStr = SmanContent.find("a", href="/setup.ps3").text
+    PS3Data['Firmware'] = ' '.join(re.search("[0-9].[0-9]{2}(.*)", re.search("(.+?)\n(.+?)", TempStr).group(1)).group(0).split(' ')[0:2])
+
     return PS3Data
-
-with open("webMAN-page/webMAN.html", "r") as file:
-    print(SmanHTMLParser(file.read()))
-
-"""
-
-class webMANParser(HTMLParser):
-    checkTag = False
-    valueIndex = 0
-    ignoreChild = 0
-
-    def handle_starttag(self, tag, attrs):
-        if self.checkTag and tag != 'a': self.ignoreChild += 1;return
-        if tag != 'a': return
-
-        # Here we'll check for distinctive attributes of the tags we want
-        # to retrieve data from i.e. CPU temps, current game, etc
-        attributes = dict(attrs)
-
-        match attributes:
-            # Title ID
-            case {'target': '_blank'}:
-                self.checkTag = True
-                self.valueIndex = 1
-
-            # Title Name
-            case {'href': url} if url.startswith('http://google.com/'):
-                self.checkTag = True
-                self.valueIndex = 2
-
-            # Temps in Celsius
-            case {'href': '/cpursx.ps3?up'}:
-                self.checkTag = True
-                self.valueIndex = 3
-
-            # Temps in Fahrenheit
-            case {'href': '/cpursx.ps3?dn'}:
-                self.checkTag = True
-                self.valueIndex = 5
-
-            # On the XMB ?
-            case {'href': '/browser.ps3$slaunch'}:
-                self.checkTag = True
-                self.valueIndex = 7
-
-            # Fan Speed
-            case {'href': '/cpursx.ps3?mode'}:
-                self.checkTag = True
-                self.valueIndex = 8
-
-            # Firmware version
-            case {'href': '/setup.ps3'}:
-                self.checkTag = True
-                self.valueIndex = 9
-
-    def handle_endtag(self, tag):
-            if not self.checkTag: return
-            if tag != 'a': self.ignoreChild -= 1; return
-            
-            self.checkTag = False
-            self.valueIndex = 0
-
-    def handle_data(self, data):
-        if not self.checkTag or self.ignoreChild != 0 or not data.strip(): return
-
-        match self.valueIndex:
-            case 1:
-                PS3Data['TitleID'] = data.strip()
-            case 2:
-                PS3Data['TitleName'] = re.search("(.+)[0-9]{2}.[0-9]{2}", data.strip()).group(1).strip() if re.search("(.+)[0-9]{2}.[0-9]{2}", data.strip()) != None else data.strip()
-            case 3:
-                PS3Data['CPUTemp']['C'] = re.search("[0-9]+", data.strip()).group(0)
-                self.valueIndex += 1
-            case 4:
-                PS3Data['RSXTemp']['C'] = re.search("[0-9]+", data.strip()).group(0)
-            case 5:
-                PS3Data['CPUTemp']['F'] = re.search("[0-9]+", data.strip()).group(0)
-                self.valueIndex += 1
-            case 6:
-                PS3Data['RSXTemp']['F'] = re.search("[0-9]+", data.strip()).group(0)
-            case 7:
-                PS3Data['TitleName'] = 'XMB'
-            case 8:
-                PS3Data['FanSpeed'] = re.search("[0-9]+", data.strip()).group(0)
-            case 9:
-                PS3Data['Firmware'] = ' '.join(re.search("[0-9].[0-9]{2}(.*)", data.strip()).group(0).split(' ')[0:2])
-"""
