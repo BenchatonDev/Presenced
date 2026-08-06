eae89e1eecf814bc4cb1ec82409bb6b7ebf5c04f
BenchatonDev <benchaton.annimation@gmail.com>
2026-08-06 23:07:26 +0200

    Token & AppID Checking

--- src/ConfigHandler.py
+++ src/ConfigHandler.py
@@ -1,11 +1,10 @@
 from PS3.PS3Client import PS3Client
-from os import path
 import json
 
 # Look a constant, in this economy ???
 DEFAULT_CONFIG = {
     "General": {
-        "AppID": "DISCORD_APP_ID_HERE",
+        "AppID": "1534965343422906582",
         "DiscordPyToken": "DISCORD_USER_TOKEN_HERE",
         "PollInterval": 20,
         "ConsoleClients": "all",
@@ -55,16 +54,41 @@ DEFAULT_CONFIG = {
 }
 
 Config = {}
-
 # TM for TradeMark, Have fun naming your most private of functions with silly names too~
 def SanitizerTM(config: dict):
     SanitizedConf = {}
 
-    def AppIDChecker():
-        return
+    def AppIDChecker(AppID: str):
+        AppID = "NotAnID" if AppID.strip() == "" else AppID
+        from requests import get
+        ValidID = ""
 
-    def UserTokenChecker():
-        return
+        if get(f"https://discord.com/api/v10/applications/{AppID}/rpc").status_code != 200:
+            print("The provided AppID isn't valid, a valid AppID is required by Presenced in order to function")
+            ValidID = AppIDChecker(input("Please enter a valid AppID: ").strip())
+        else:
+            ValidID = AppID
+
+        return ValidID
+
+    def UserTokenChecker(UserToken: str):
+        UserToken = "NotAToken" if UserToken.strip() == "" else UserToken
+        from discord import Client
+        Connection = Client()
+        ValidToken = ""
+
+        @Connection.event
+        async def on_ready():
+            await Connection.close()
+
+        try:
+            Connection.run(UserToken, log_handler=None)
+            ValidToken = UserToken
+        except:
+            print("The provided UserToken isn't valid, a valid UserToken is required by Presenced in order to function standalone with discord.py-self")
+            ValidToken = UserTokenChecker(input("Please enter a valid UserToken: "))  
+
+        return ValidToken
 
     def TelePrompter():
         return
