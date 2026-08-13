d45032f41e65a94195ba84c8b4ffa7f6e4605e18
BenchatonDev <benchaton.annimation@gmail.com>
2026-08-14 00:31:29 +0200

    Fixes and other stuff

--- src/PS3/PS3Client.py
+++ src/PS3/PS3Client.py
@@ -1,8 +1,10 @@
 from .SmanParser import SmanHTMLParser
 from ConsoleClient import ConsoleClient, ActiveClients
+from datetime import datetime, timezone
 from icmplib import ping
 from copy import deepcopy
 from requests import get
+
 
 RequestHeaders = { "User-Agent": "Mozilla/5.0" }
 
@@ -14,18 +16,18 @@ class PS3Client(ConsoleClient):
                 get(f"http://{self.Config["PS3Address"]}", headers=RequestHeaders, timeout=1)
 
             except:
-                ActiveClients.remove(self); self.ClientData["ConsoleData"], self.ClientData["OldConsoleData"] = {}, {} \
-                if self in ActiveClients else None
+                if self in ActiveClients: ActiveClients.remove(self); \
+                    self.ClientData["ConsoleData"], self.ClientData["OldConsoleData"] = {}, {}
 
                 return
             
             else:
-                ActiveClients.insert(0, self); self.ClientData["AppStartTime"] = 10 \
-                if self not in ActiveClients else None
+                if self not in ActiveClients: ActiveClients.insert(0, self); \
+                    self.ClientData["AppStartTime"] = int(datetime.now(timezone.utc).timestamp())
 
                 SmanHTML = get(f"http://{self.Config["PS3Address"]}/cpursx.ps3?/sman.ps3", headers=RequestHeaders).text
 
-                self.ClientData["OldConsoleData"] = deepcopy(self.ClientData)
+                self.ClientData["OldConsoleData"] = deepcopy(self.ClientData["ConsoleData"])
                 self.ClientData["ConsoleData"] = SmanHTMLParser(SmanHTML)
 
         else:
