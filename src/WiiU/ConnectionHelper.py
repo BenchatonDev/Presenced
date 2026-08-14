00f17af8b24e0e1e19301db9384acec6607b3405
BenchatonDev <benchaton.annimation@gmail.com>
2026-08-14 16:09:13 +0200

    Data collection go brrr

---
+++ src/WiiU/ConnectionHelper.py
@@ -0,0 +1,48 @@
+from socket import (socket, AF_INET, SOCK_DGRAM)
+from time import sleep; from json import (loads, JSONDecodeError)
+
+Connection = socket(AF_INET, SOCK_DGRAM)
+Connection.settimeout(1)
+Bound = False
+
+def WiiUConnector(WiiUPort: int, Retries: int = 5, Delay: int = 0):
+    global Connection, Bound
+
+    while Retries > 0:
+        if not Bound:
+            try:
+                Connection.bind(("", WiiUPort))
+                Bound = True
+            except:
+                sleep(Delay)
+                Retries -= 1
+                continue
+
+        try:
+            # They used 1024 buffer sized in the StackOverflow page I read so I kept it that way
+            UDPData, Address = Connection.recvfrom(1024)
+            if UDPData and len(UDPData) > 0:
+                UDPData = UDPData.decode('utf-8', errors='ignore')
+                try:
+                    WiiUData = loads(UDPData)
+                                
+                    if WiiUData.get("sender") == "Wii U":                 
+                        WiiUData.pop("sender")
+                        return WiiUData
+                    else:
+                        raise Exception
+                                    
+                except JSONDecodeError:
+                    sleep(Delay)
+                    Retries -= 1
+                    continue
+
+            else:
+                raise Exception
+
+        except:
+            sleep(Delay)
+            Retries -= 1
+            continue
+    
+    return {}
