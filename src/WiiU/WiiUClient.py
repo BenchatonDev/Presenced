fb1ba2b8714aa6c278f607a987c86599137a1707
BenchatonDev <benchaton.annimation@gmail.com>
2026-08-14 17:09:03 +0200

    Wii U ping ✅

--- src/WiiU/WiiUClient.py
+++ src/WiiU/WiiUClient.py
@@ -1,14 +1,30 @@
+from .ConnectionHelper import WiiUConnector
 from ConsoleClient import ConsoleClient, ActiveClients
-from icmplib import ping
+from datetime import datetime, timezone
 from copy import deepcopy
 
 
 class WiiUClient(ConsoleClient):
 
     def pingConsole(self):
+        WiiUData = WiiUConnector(self.Config["UDPPort"])
+
+        if WiiUData:
+            if self not in ActiveClients: ActiveClients.insert(0, self); \
+                self.ClientData["AppStartTime"] = int(datetime.now(timezone.utc).timestamp())
+
+            if self.Config["Presence"]["ResetTimeOnAppChange"]:
+                self.ClientData["AppStartTime"] = WiiUData["Time"]
+            WiiUData.pop("Time")
+            
+            self.ClientData["OldConsoleData"] = deepcopy(self.ClientData["ConsoleData"])
+            self.ClientData["ConsoleData"] = deepcopy(WiiUData)
+
+        else:
+            if self in ActiveClients: ActiveClients.remove(self); \
+                self.ClientData["ConsoleData"], self.ClientData["OldConsoleData"] = {}, {}
 
         return
 
     def getRPCData(self):
-            
-        return
+        return {}
